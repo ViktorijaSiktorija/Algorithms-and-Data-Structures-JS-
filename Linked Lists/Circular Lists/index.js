4cc0f85a863264840f03dadcf40f42112107ca41
ViktorijaSiktorija <51799486+ViktorijaSiktorija@users.noreply.github.com>
2021-04-14 12:10:55 +0200

    Update index.js

--- Linked Lists/Circular Lists/index.js
+++ Linked Lists/Circular Lists/index.js
@@ -12,14 +12,6 @@
 //   c.next = b;
 //   circular(l) // true
 
-// circular - nema tail node, poslednji node koji mozda slucajno pokazuje na raniji nod u LL
-// ovde nema null tj tail pokazuje na null, iteracija bi bila beskonacna
-// 2 pointera, slow i fast, slow da gleda u 1. nod i fast da gleda u sledeci
-// ako su sledeca 2 posle fast defined, ulazi u for loop, 
-// slow ide po 1 napred
-// onda check, da li slow i fast gledaju u isti nod, u drugom slucaju nastavlja se iteracija
-// ako jesu defined za fast ide dalje 2, slow napred po 1, check dal gledaju u isti nod
-// ne gledaju, ponovo iteracija sve isto, ako gledjau oba u isto vreme znaci da imamo circ LL
 
 function circular(list) {
     let slow = list.getFirst();
@@ -29,7 +21,7 @@ function circular(list) {
         slow = slow.next;
         fast = fast.next.next;
 
-        if (slow === fast) { // dal je identicni nod
+        if (slow === fast) { 
             return true;
         }
     }
