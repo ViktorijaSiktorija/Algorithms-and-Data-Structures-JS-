b7699b799ddbc287819e0ca92923cc67d0109ca5
ViktorijaSiktorija <51799486+ViktorijaSiktorija@users.noreply.github.com>
2021-04-14 12:11:25 +0200

    Update index.js

--- Linked Lists/Find The Midpoint/index.js
+++ Linked Lists/Find The Midpoint/index.js
@@ -12,17 +12,9 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-// define center node, 2 temp promenljive slow i fast
-// uzmu se i povezu sa prvim node, asign slow = 1. node i fast = 1. node
-// iterate kroz LL, za svaki korak iteracije (while) slow ce ici 1 el napred
-// a fast napred kroz 2 elementa
-// kako fast dodje do sledeceg, proveravamo dalje sledeci node defined i node posle tog, tj dal postoje (next to fast)
-// ako postoje nastavljamo iteraciju, ako ne postoje fast je verovatno na kraju LL, slow je na mid point
-// kada fast dodje do poslednjeg nisu dalje defined, znaci na kraju smo ll, slow je na sredini jer se pomera duplo sporije u odnosu na slow
-// nebitno dal su parni il neparni node
 
 function midpoint(list) {
-    let slow = list.getFirst(); //ili list.head
+    let slow = list.getFirst(); 
     let fast = list.getFirst();
 
     while (fast.next && fast.next.next) {
