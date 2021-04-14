a5f45d4d47e09f17cf9be768640c539a7ad8db28
ViktorijaSiktorija <51799486+ViktorijaSiktorija@users.noreply.github.com>
2021-04-14 12:16:04 +0200

    Update index.js

--- String Reversal/index.js
+++ String Reversal/index.js
@@ -6,29 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-// First solution, tako uradila
+// First solution
 
-// function reverse(str) {
-//     return str.split("").reverse().join("");
-// }
-// console.log(reverse("hello"));
+function reverse(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverse("hello"));
 
 // Second; for of loop
 
 // empty string called "reversed"
 // for each character in provided string
-//  take the character and add it to the start of "reversed"
+// take the character and add it to the start of "reversed"
 // return the var "reversed"
 
-// function reverse(str) {
-//     let reversed = "";
+function reverse(str) {
+    let reversed = "";
 
-//     for (let character of str){
-//         reversed = character + reversed;
-//     }
-//     return reversed;
-// }
-// console.log(reverse("hello"));
+    for (let character of str){
+        reversed = character + reversed;
+    }
+    return reversed;
+}
+console.log(reverse("hello"));
 
 // Third; reduce helper
 
